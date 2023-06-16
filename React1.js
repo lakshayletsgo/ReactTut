07fd83e4edf719e3c9f9baaa906f40061eeeaec4
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-06-16 19:17:23 +0530

    React 1

---
+++ React1.js
@@ -0,0 +1,14 @@
+// This is the first react app writing page 
+// We use npx to build this folder because it temporarily installs the library to create a folder while npm install the entire library and then build it 
+// In react we use jsx which is html 
+// It is a little different then html. There are only few lines where we have different name of the component
+// In this path i will upload the code at the end of this file after the code summary
+
+
+// The main file in react is App.js as it is one which is reloaded on the page
+// The App.js is rendered using the help of JavaScript applied to a site 
+// There are two types of component in React 
+// 1. Function 
+// 2. Class
+
+// We will mostly use the function component and it can only return one element so will always enclose the entire jsx in tag
