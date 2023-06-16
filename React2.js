6b8fbd89dba32c0a308464f2138790f1833d4391
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-06-17 02:15:13 +0530

    Default and name export in Js

---
+++ React2.js
@@ -0,0 +1,23 @@
+// In React Code we can only return 1 tag so either we have to close entire tag in one jsx fragment<></> like this
+// Or we can just return one <div></div> 
+// To add bootstrap just add it in index.html page and it will be there
+// There are two types of export 
+// 1. Name Export
+// 2. Default Export
+
+// Jam ham name export karte hai toh jam ham normal import karte hai toh voh import
+// const a = 10;
+// const b = 20;
+// export default b;
+
+// import ca from 'xyz';
+// Isse b hi import hoga 
+
+
+// Aur jam ham just aise hi export karte hai toh jo hame uske name ke saath hi import karna padta hai 
+// const a = 10;
+// const b = 10;
+// export {a};
+
+// import a from 'xyz';
+// Ab agr a ko import karna hai toh aise hi karna padega
