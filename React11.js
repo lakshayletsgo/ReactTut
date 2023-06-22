9da54b24466c9d31c8caf2e4e725a30449b0cae5
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-06-22 23:49:54 +0530

    Class Component Project

---
+++ React11.js
@@ -0,0 +1,15 @@
+// This is the description of the project I have done so far
+// Today is day 2 of making NewsMonkey
+// Today we have added cards to add news which contains our news element
+// We have used new api to get news 
+// We have put the sample news to the outputSample.json file
+// News will be recieved in a json format
+// Now we have called props using this method
+
+// let {title,description} = this.props
+
+// This will get props in the name of title and description 
+// We also got photos link in the json file 
+// We can use this link to get the photo and put it in the cards of the news
+// We will only make changes in the NewsItem component
+// And we will get it changed all over other component 
