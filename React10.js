6ad283292f3711c9a853fd8d17d38a1af4a69845
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-06-22 13:35:58 +0530

    Class Base Component

---
+++ React10.js
@@ -0,0 +1,19 @@
+// Class base component 
+// This is how you can use class component
+
+
+// Syntax
+// import React, { Component } from 'react'
+// import Navbar from './components/Navbar'
+// import News from './components/News'
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//       </div>
+//     )
+//   }
+// }
+
+// It helps in using methods easily
