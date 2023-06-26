445b4b53ea577c48a86997479bb24223f5f0b122
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-06-26 12:48:21 +0530

    Hooks

---
+++ React19.js
@@ -0,0 +1,16 @@
+// Hooks in React 
+// We use hook so that we can use the functions of class component in function component
+
+// There are mainly 4 functions of hooks
+// useState
+// useEffect
+// useContext
+// useRef
+
+// useState is used to state in functions
+// useEffect is genenerally used in componentDidMount function and componentDidUpdate
+// useRef is used to refer a space in DOM 
+// useContext will be used later 
+// We can use class and function component in same project
+// After this we will start learning about MERN stack 
+// We will learn about it so that we can make our own api 
